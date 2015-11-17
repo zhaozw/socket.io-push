0c2297a88c85c56738832cb5d7af65d7133bf6f8
xuudoo <xuudoo@gmail.com>
2015-11-17 10:53:39 +0800

    add nohupDebug

---
+++ lbs-socketio/nohupDebug.sh
@@ -0,0 +1,11 @@
+kill `cat pid_debug`
+
+while [[ ${?} == 0 ]]      # Repeat until the process has terminated.
+do
+    sleep 10s              # Wait a bit before testing.
+    ps -p `cat pid_debug` >/dev/null  # Check if the process has terminated.
+done
+
+export DEBUG=ProxyServer,RedisStore
+nohup node . > debug.log 2>&1&
+echo $! > pid_debug
