4c0201b8d02b768aecee15a4ca05e2edab8275b7
root <root@ubuntu.(none)>
2015-11-19 18:17:15 +0800

    add config

--- lbs-socketio/config.js
+++ lbs-socketio/config.js
@@ -1,6 +1,20 @@
 var config = {};
-config.redis = {};
-config.redis.port = 6379;
-config.redis.host = localhost;
+
+config.redis = {
+  host : "localhost",
+  port : 6379
+};
+
+config.io_1 = {
+  port : 9101
+};
+
+config.api_1 = {
+  port : 9102
+};
+
+config.apn = {
+  production : true
+};
 
 module.exports = config;
