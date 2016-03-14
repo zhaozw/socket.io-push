c0540a008ae72c4d7a8cc9d202f2bf938092d4c2
许铎 <xuduo@yy.com>
2016-03-14 17:27:38 +0800

    拆分index.js

--- push-server/index.js
+++ push-server/index.js
@@ -12,32 +12,7 @@ if (!program.instance) {
     program.instance = 1;
 }
 var config = require(process.cwd() + "/config");
-console.log("config " + JSON.stringify(config));
 var instance = program.instance;
 
-console.log("starting instance #" + instance);
-var ioPort = config.io_port + instance - 1;
-var apiPort = config.api_port + instance - 1;
-
-
-var simpleRedisHashCluster = require('./lib/redis/simpleRedisHashCluster.js');
-
-new simpleRedisHashCluster(config.redis, function (cluster) {
-
-    var io = require('socket.io')(ioPort, {pingTimeout: config.pingTimeout, pingInterval: config.pingInterval});
-    console.log("start server on port " + ioPort);
-    var socketIoRedis = require('socket.io-redis')({pubClient: cluster, subClient: cluster});
-    io.adapter(socketIoRedis);
-    var packetService = require('./lib/service/packetService.js')(cluster, cluster);
-    var Stats = require('./lib/stats/stats.js');
-    var stats = new Stats(cluster, ioPort);
-    var uidStore = require('./lib/redis/uidStore.js')(cluster);
-    var TtlService = require('./lib/service/ttlService.js');
-    var ttlService = new TtlService(cluster);
-    var notificationService = require('./lib/service/notificationService.js')(config.apns, cluster, ttlService);
-
-    require('./lib/server/proxyServer.js')(io, stats, packetService, notificationService, uidStore, ttlService);
-
-    var restApi = require('./lib/api/restApi.js')(io, stats, notificationService, apiPort, uidStore, ttlService, cluster);
-});
+require('./lib/push-server.js')(config);
 
---
+++ push-server/lib/push-server.js
@@ -0,0 +1,33 @@
+module.exports = PushServer;
+
+function PushServer(config) {
+    console.log("config " + JSON.stringify(config));
+    var instance = config.instance || 1;
+    console.log("starting instance #" + instance);
+    var ioPort = config.io_port + instance - 1;
+    var apiPort = config.api_port + instance - 1;
+
+
+    var simpleRedisHashCluster = require('./redis/simpleRedisHashCluster.js');
+
+    new simpleRedisHashCluster(config.redis, function (cluster) {
+
+        var io = require('socket.io')(ioPort, {pingTimeout: config.pingTimeout, pingInterval: config.pingInterval});
+        console.log("start server on port " + ioPort);
+        var socketIoRedis = require('socket.io-redis')({pubClient: cluster, subClient: cluster});
+        io.adapter(socketIoRedis);
+        var packetService = require('./service/packetService.js')(cluster, cluster);
+        var Stats = require('./stats/stats.js');
+        var stats = new Stats(cluster, ioPort);
+        var uidStore = require('./redis/uidStore.js')(cluster);
+        var TtlService = require('./service/ttlService.js');
+        var ttlService = new TtlService(cluster);
+        var notificationService = require('./service/notificationService.js')(config.apns, cluster, ttlService);
+
+        require('./server/proxyServer.js')(io, stats, packetService, notificationService, uidStore, ttlService);
+
+        var restApi = require('./api/restApi.js')(io, stats, notificationService, apiPort, uidStore, ttlService, cluster);
+    });
+}
+
+
